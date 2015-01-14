8e99d913f0b5b384df29931c8e38eebb7e1576e4
Kevin Montuori <montuori@gmail.com>
2015-01-14 17:56:34 -0600

    moved action enum implementation back to being generated by macro

--- lib/digital_ocean/actions.ex
+++ lib/digital_ocean/actions.ex
@@ -18,43 +18,7 @@ defmodule DigitalOcean.Actions do
 
   defstruct(actions: [], links: %{}, meta: %{})
   Macros.define_as_struct(:actions, DigitalOcean.Actions.Action)
-
+  Macros.implement_enumerable(:actions, DigitalOcean.Actions)
   def get_next_page(url), do: as_struct(DigOc.page!(url))
-
-
-  defimpl Enumerable, for: __MODULE__ do
-    def count(c) do
-      case Application.get_env(:digital_ocean, :use_api_paging, false) do
-        true -> {:ok, c.meta.total}
-        false -> {:ok, length(c.actions) }
-      end
-    end
-      
-    def member?(c, v) do
-      {:ok, Enum.any?(c.actions, fn(x) -> x.id == v end) }
-    end
-    
-    def reduce(_, {:halt, acc}, _), do: {:halted, acc}
-    def reduce(c, {:suspend, acc}, fun) do
-      {:suspended, acc, &reduce(c, &1, fun)}
-    end
-
-    # item list is empty, maybe retrieve the next page
-    def reduce(%{:actions => []} = c, {:cont, acc}, fun) do
-      if (Map.has_key?(c.links, :pages)
-          && Map.has_key?(c.links.pages, :next)
-          && Application.get_env(:digital_ocean, :use_api_paging, false)) do
-        c = DigitalOcean.Actions.get_next_page(c.links.pages.next)
-        reduce(c, {:cont, acc}, fun)
-      else
-        {:done, acc}
-      end
-    end
-
-    # draining down the entry list
-    def reduce(%{:actions => [h|t]} = c, {:cont, acc}, fun) do
-      reduce(%{ c | :actions => t }, fun.(h, acc), fun)
-    end
-  end
   
 end
--- lib/digital_ocean/macros.ex
+++ lib/digital_ocean/macros.ex
@@ -14,26 +14,57 @@ defmodule DigitalOcean.Macros do
   defmacro implement_enumerable(key, type) do
     quote do
       defimpl Enumerable, for: unquote(type) do
-        def count(c), do: {:ok, length(c[unquote(key)])}
 
+        def count(c) do
+          if Application.get_env(:digital_ocean, :use_api_paging, false) do
+            {:ok, c.meta.total}
+          else
+            {:ok, length(c[unquote(key)])}
+          end
+        end
+        
+        
+        def member?(c, v) when is_integer(v) do
+          {:ok, Enum.any?(c[unquote(key)], fn(x) -> x.id == v end) }
+        end
+        
         def member?(c, v) when is_binary(v) do
           {:ok, Enum.any?(c[unquote(key)], fn(x) -> x.slug == v end) }
         end
 
-        def member?(c, v) when is_integer(v) do
-          {:ok, Enum.any?(c[unquote(key)], fn(x) -> x.id == v end) }
-        end
-
+        
         def reduce(_, {:halt, acc}, _), do: {:halted, acc}
+          
         def reduce(c, {:suspend, acc}, fun) do
           {:suspended, acc, &reduce(c, &1, fun)}
         end
-        def reduce(%{unquote(key) => []}, {:cont, acc}, _fun) do
-          {:done, acc}
+        
+        def reduce(%{unquote(key) => []} = c, {:cont, acc}, fun) do
+          if (Map.has_key?(c.links, :pages)
+              && Map.has_key?(c.links.pages, :next)
+              && Application.get_env(:digital_ocean, :use_api_paging, false)) do
+            c = apply(unquote(type), :get_next_page, [c.links.pages.next])
+            reduce(c, {:cont, acc}, fun)
+          else
+            {:done, acc}
+          end
         end
+
         def reduce(%{unquote(key) => [h|t]} = c, {:cont, acc}, fun) do
           reduce(%{ c | unquote(key) => t }, fun.(h, acc), fun)
         end
+        
+        #   def reduce(_, {:halt, acc}, _), do: {:halted, acc}
+        #   def reduce(c, {:suspend, acc}, fun) do
+        #     {:suspended, acc, &reduce(c, &1, fun)}
+        #   end
+        #   def reduce(%{unquote(key) => []}, {:cont, acc}, _fun) do
+        #     {:done, acc}
+        #   end
+        #   def reduce(%{unquote(key) => [h|t]} = c, {:cont, acc}, fun) do
+        #     reduce(%{ c | unquote(key) => t }, fun.(h, acc), fun)
+        #   end
+        # end
       end
     end
   end
