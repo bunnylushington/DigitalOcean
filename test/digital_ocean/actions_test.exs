9037c61970980f7e5e1162ce62f38341f9352f47
Kevin Montuori <montuori@gmail.com>
2015-01-14 10:11:33 -0600

    more action tests implemented

--- test/digital_ocean/actions_test.exs
+++ test/digital_ocean/actions_test.exs
@@ -20,5 +20,19 @@ defmodule DigitalOcean.ActionsTest do
     assert s[:resource_type] == "droplet"
   end
   
-  
+  test "actions struct created from fixture", %{ fixtures: actions } do
+    s = struct(DigitalOcean.Actions, actions)
+    assert length(s.actions) == 3
+    refute s.links == %{}
+    assert s.meta[:total] == 1145
+  end
+
+  test "struct and embedded structs created", %{ fixtures: actions } do
+    s = DigitalOcean.Actions.as_struct(actions)
+    assert length(s.actions) == 3
+    action = hd(s.actions)
+    assert action.__struct__ == DigitalOcean.Actions.Action
+    assert action.region == "nyc3"
+  end
+   
 end
