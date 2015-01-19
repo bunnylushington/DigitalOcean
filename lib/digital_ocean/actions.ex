7aa78cb7d23eb5f03c420e23ea381cb5c2a666c7
Kevin Montuori <montuori@gmail.com>
2015-01-19 10:59:37 -0600

    augmented action documentation

--- lib/digital_ocean/actions.ex
+++ lib/digital_ocean/actions.ex
@@ -3,9 +3,43 @@ defmodule DigitalOcean.Actions do
   @derive [Access]
   require DigitalOcean.Macros, as: Macros
 
-  defmodule Action do
-    @derive [Access]
 
+  defstruct(actions: [], links: %{}, meta: %{})
+  @type t :: %__MODULE__{actions: [DigitalOcean.Actions.Action],
+                         links: DigitalOcean.links,
+                         meta: DigitalOcean.meta}
+  
+  Macros.define_as_struct(:actions, DigitalOcean.Actions.Action)
+  Macros.implement_enumerable(:actions, DigitalOcean.Actions)
+
+  defmodule Action do
+    @moduledoc """
+    Single Digital Ocean Action Struct
+
+    Its fields are:
+
+      * `id` [integer] - unique identifier
+
+      * `status` [string] - one of "in-progress", "completed", "errored"
+
+      * `type` [string] - type of action represented
+
+      * `started_at` [string] - ISO8601 when action was initiated
+
+      * `completed_at` [string] - ISO8601 when action was completed
+
+      * `resource_id` [integer] - id for the resouce action is associated with
+
+      * `resource_type` [string] - type of resource action is associated with
+
+      * `region` [string] - slug representing region where action occured
+
+    See [the Digial Ocean Action
+    documentation](https://developers.digitalocean.com/#actions) for
+    full details.
+    """
+    @derive [Access]
+    
     defstruct(id: nil,
               status: nil,
               type: nil,
@@ -26,12 +60,4 @@ defmodule DigitalOcean.Actions do
                           
   end
 
-  defstruct(actions: [], links: %{}, meta: %{})
-  @type t :: %__MODULE__{actions: [DigitalOcean.Actions.Action],
-                         links: DigitalOcean.links,
-                         meta: DigitalOcean.meta}
-  
-  Macros.define_as_struct(:actions, DigitalOcean.Actions.Action)
-  Macros.implement_enumerable(:actions, DigitalOcean.Actions)
-  
 end
