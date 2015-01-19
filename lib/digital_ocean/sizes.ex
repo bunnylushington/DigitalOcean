e7385e82b96271aeaa0779ef97ffb2866933baa9
Kevin Montuori <montuori@gmail.com>
2015-01-19 10:54:41 -0600

    augmented size documentation

--- lib/digital_ocean/sizes.ex
+++ lib/digital_ocean/sizes.ex
@@ -3,7 +3,40 @@ defmodule DigitalOcean.Sizes do
   @derive [Access]
   require DigitalOcean.Macros, as: Macros
 
+  defstruct(sizes: [], links: %{}, meta: %{})
+  @type t :: %__MODULE__{sizes: [DigitalOcean.Sizes.Size.t],
+                         links: DigitalOcean.links,
+                         meta: DigitalOcean.meta}
+  
+  Macros.define_as_struct(:sizes, DigitalOcean.Sizes.Size)
+  Macros.implement_enumerable(:sizes, DigitalOcean.Sizes)
+  
   defmodule Size do
+    @moduledoc """
+    Single Digital Ocean Size Struct
+
+    Its fields are:
+
+      * `slug` [string] - unique human readable identifier
+
+      * `transfer` [number] - amount of transfer bandwidth (TB)
+
+      * `price_monthly` [float] - monthly cost of droplet (USD)
+
+      * `price_hourly` [float] - hourly cost of droplet (USD)
+
+      * `memory` [integer] - amount of RAM (MB)
+
+      * `vcpus` [integer] - number of virtual CPUs
+
+      * `disk` [integer] - disk space set aside (GB)
+
+      * `regions` [array] - region slugs where this size is available
+
+    See [the Digital Ocean Sizes
+    documentation](https://developers.digitalocean.com/#sizes) for
+    full details.
+    """
     @derive [Access]
     defstruct(disk:          nil,
               memory:        nil,
@@ -23,14 +56,5 @@ defmodule DigitalOcean.Sizes do
                            disk: integer,
                            regions: [DigitalOcean.slug]}
   end
-
-  defstruct(sizes: [], links: %{}, meta: %{})
-  @type t :: %__MODULE__{sizes: [DigitalOcean.Sizes.Size.t],
-                         links: DigitalOcean.links,
-                         meta: DigitalOcean.meta}
-  
-  Macros.define_as_struct(:sizes, DigitalOcean.Sizes.Size)
-  Macros.implement_enumerable(:sizes, DigitalOcean.Sizes)
-    
 end
 
