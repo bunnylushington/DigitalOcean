1f903f8d19bb2d835ad88d142410c33b84f74961
Kevin Montuori <montuori@gmail.com>
2015-02-04 12:53:46 -0600

    added a to_struct method to match other modules

--- lib/digital_ocean/kernels.ex
+++ lib/digital_ocean/kernels.ex
@@ -12,10 +12,12 @@ defmodule DigitalOcean.Kernels do
 
        * `version` [string]    
     """
-    
+
     @derive [Access]
     defstruct(id: nil, name: nil, version: nil)
     @type t :: %__MODULE__{id: integer, name: String.t, version: String.t}
+
+    def to_struct(data), do: struct(__MODULE__, data)
     
   end
 end
