7fae244cdecfec563ecfd2116e91758939682d9d
Kevin Montuori <montuori@gmail.com>
2015-02-04 10:29:39 -0600

    fixed typo, formatting

--- test/digital_ocean/domains_test.exs
+++ test/digital_ocean/domains_test.exs
@@ -6,20 +6,20 @@ defmodule DigitalOcean.DomainsTest do
     {:ok, fixtures: data}
   end
 
-  test "domain struct created from fixture", %{ fixtures: d } do
+  test "domain struct created from fixture", %{fixtures: d} do
     data = hd(d.domains)
     s = struct(DigitalOcean.Domains.Domain, data)
     assert s[:name] == "example.com"
     assert s.ttl == 1800
   end
 
-  test "domains struct created form fixture", %{ fixtures: d } do
+  test "domains struct created from fixture", %{fixtures: d} do
     s = struct(DigitalOcean.Domains, d)
     assert length(s.domains) == s.meta[:total]
     assert s.links == %{}
   end
 
-  test "struct and embedded structs created", %{ fixtures: d } do
+  test "struct and embedded structs created", %{fixtures: d} do
     {:ok, s} = DigitalOcean.Domains.as_struct(d)
     assert length(s.domains) == s.meta[:total]
     domain = hd(s.domains)
@@ -27,14 +27,14 @@ defmodule DigitalOcean.DomainsTest do
     assert domain.name == "example.com"
   end
 
-  test "domains as enumeration", %{ fixtures: d } do
+  test "domains as enumeration", %{fixtures: d} do
     {:ok, s} = DigitalOcean.Domains.as_struct(d)
     assert Enum.count(s) == s.meta[:total]
     assert Enum.member?(s, "example.com")
     refute Enum.member?(s, "notexample.com")
   end
 
-  test "domains as iteration", %{ fixtures: d } do
+  test "domains as iteration", %{fixtures: d} do
     {:ok, s} = DigitalOcean.Domains.as_struct(d)
     res = for _ <- s, do: :ok
     assert res == List.duplicate(:ok, Enum.count(s))
