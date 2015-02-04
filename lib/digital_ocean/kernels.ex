897824952e0e7f0cdd3dd98b3a5143c05ef2d1ab
Kevin Montuori <montuori@gmail.com>
2015-02-04 11:50:40 -0600

    kernel struct and test created

---
+++ lib/digital_ocean/kernels.ex
@@ -0,0 +1,22 @@
+defmodule DigitalOcean.Kernels do
+  
+  defmodule Kernel do
+    @moduledoc """
+    Digital Ocean Kernel Struct
+   
+    Its fields are
+
+       * `id` [integer]
+   
+       * `name` [string]
+
+       * `version` [string]    
+    """
+    
+    @derive [Access]
+    defstruct(id: nil, name: nil, version: nil)
+    @type t :: %__MODULE__{id: integer, name: String.t, version: String.t}
+    
+  end
+end
+  
---
+++ test/digital_ocean/kernels_test.exs
@@ -0,0 +1,17 @@
+defmodule DigitalOcean.KernelsTest do
+  use ExUnit.Case
+
+  setup do
+    {data, _} = Code.eval_file("test/sample-data/droplets", System.cwd)
+    {:ok, fixtures: data}
+  end
+  
+  test "test kernel struct creation", %{fixtures: d} do
+    kernels = for droplet <- d.droplets do
+      struct(DigitalOcean.Kernels.Kernel, droplet.kernel)
+    end
+    assert Enum.all?(kernels,
+      fn(k) -> k.__struct__ == DigitalOcean.Kernels.Kernel end)
+  end
+  
+end
