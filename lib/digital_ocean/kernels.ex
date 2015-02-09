dd6b9df5851b3ed464a4fd1a7edb3619a6455397
Kevin Montuori <montuori@gmail.com>
2015-02-09 09:47:38 -0600

    add kernel struct, enum, fixtures

--- lib/digital_ocean/kernels.ex
+++ lib/digital_ocean/kernels.ex
@@ -1,4 +1,17 @@
 defmodule DigitalOcean.Kernels do
+
+  @derive [Access]
+  require DigitalOcean.Macros, as: Macros
+
+  defstruct(kernels: [], links: %{}, meta: %{})
+
+  @type t :: %__MODULE__{kernels: [DigitalOcean.Kernels.Kernel.t],
+                         links: DigitalOceanlinks,
+                         meta: DigitalOcean.meta}
+
+  Macros.define_as_struct(:kernels, DigitalOcean.Kernels.Kernel)
+  Macros.implement_enumerable(:kernels, DigitalOcean.Kernels)
+
   
   defmodule Kernel do
     @moduledoc """
--- test/digital_ocean/kernels_test.exs
+++ test/digital_ocean/kernels_test.exs
@@ -3,15 +3,24 @@ defmodule DigitalOcean.KernelsTest do
 
   setup do
     {data, _} = Code.eval_file("test/sample-data/droplets", System.cwd)
-    {:ok, fixtures: data}
+    {kerns, _} = Code.eval_file("test/sample-data/kernels", System.cwd)
+    {:ok, droplet: data, kernels: kerns}
   end
   
-  test "test kernel struct creation", %{fixtures: d} do
+  test "test kernel struct creation from droplet data", %{droplet: d} do
     kernels = for droplet <- d.droplets do
       DigitalOcean.Kernels.Kernel.as_struct(droplet.kernel)
     end
     assert Enum.all?(kernels,
       fn(k) -> k.__struct__ == DigitalOcean.Kernels.Kernel end)
   end
+
+  test "build kernel struct from kernel fixture data", %{kernels: d} do
+    {:ok, s} = DigitalOcean.Kernels.as_struct(d)
+    assert s.__struct__ == DigitalOcean.Kernels
+    k = Enum.at(s, 0)
+    assert k.__struct__ == DigitalOcean.Kernels.Kernel
+  end
+
   
 end
